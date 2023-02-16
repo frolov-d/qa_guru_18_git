ca0d921d01cc5a58a0f59b1ae06661bad37a5cfd
Dmitry F <dimfroloff@gmail.com>
2023-02-16 22:46:41 +0300

    <3 PF

--- src/main/java/com/jsd/qa_guru_18_git/Main.java
+++ src/main/java/com/jsd/qa_guru_18_git/Main.java
@@ -3,6 +3,6 @@ package com.jsd.qa_guru_18_git;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Goodbye cruel world");
     }
 }
